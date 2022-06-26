d12d1fef575f32b88cf13ee135cbf1991714439f
alaakhdour <aalkhdou@my.centennialcollege.ca>
2022-06-26 12:16:47 -0400

    connected to mongoDB

--- .history/Server/Config/db_20220626121523.ts
+++ .history/Server/Config/db_20220626121523.ts
@@ -2,6 +2,6 @@
 //Alaa Alkhdour
 //301230316
 export const LocalURI = "mongodb://127.0.0.1/books229";
-export const RemoteURI = "mongodb+srv://alakhdour:wZ1dYnIlJVutNzYH@cluster0.kbtebdd.mongodb.net/books229?retryWrites=true&w=majority";
+export const RemoteURI = "mongodb+srv://alakhdour:wZ1dYnIlJVutNzYH@cluster0.kbtebdd.mongodb.net/?retryWrites=true&w=majority";
 export const HostName = "MongoDB Atlas";
 export const Secret = "someSecret";
--- .history/Server/Config/db_20220626121527.js
+++ .history/Server/Config/db_20220626121527.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Secret = exports.HostName = exports.RemoteURI = exports.LocalURI = void 0;
 exports.LocalURI = "mongodb://127.0.0.1/books229";
-exports.RemoteURI = "mongodb+srv://alakhdour:wZ1dYnIlJVutNzYH@cluster0.kbtebdd.mongodb.net/books229?retryWrites=true&w=majority";
+exports.RemoteURI = "mongodb+srv://alakhdour:wZ1dYnIlJVutNzYH@cluster0.kbtebdd.mongodb.net/?retryWrites=true&w=majority";
 exports.HostName = "Mongo DB Atlas";
 exports.Secret = "someSecret";
 //# sourceMappingURL=db.js.map
